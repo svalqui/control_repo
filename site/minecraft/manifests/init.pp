99923cdb92d64b3b818a33b068ceb28bf88385b2
Sergio <svalqui@users.noreply.github.com>
2022-05-13 17:30:02 +1000

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,18 @@
+class minecraft {
+  file {'opt/minecraft':
+  ensure => directory,
+  }
+  file {'/opt/minecraft/minecraft_server.jar':
+  ensure => file,
+  source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+  }
+}
