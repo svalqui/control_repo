886225e0bbb9adc0dabaccbd5c939ebf02b14a85
Sergio <svalqui@users.noreply.github.com>
2022-05-16 10:47:37 +1000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,6 +5,7 @@ class minecraft {
   file {'/opt/minecraft/minecraft_server.jar':
   ensure => file,
   source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+  mode => '0774'
   }
   package {'java':
     ensure => present,
