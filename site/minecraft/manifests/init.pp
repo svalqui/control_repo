6fdefa6457a60df55393d1aabc419f77d20361e9
Sergio <svalqui@users.noreply.github.com>
2022-05-16 10:54:46 +1000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -6,6 +6,7 @@ class minecraft {
   ensure => file,
   source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   mode => '0774'
+  before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
@@ -21,5 +22,6 @@ class minecraft {
   service {'minecraft':
     ensure => running,
     enable => true,
+    require => [Package['java'], File['/opt/minecraft/eula.txt'], File['/etc/systemd/system/minecraft.service']],
   }
 }
